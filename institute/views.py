1de66a433eed78b7c6a3b5b8725ad5f65046e3e5
sreekanthkaralmanna <sreekanthkaralmanna@gmail.com>
2013-06-23 00:38:36 +0530

    institute views updated

--- institute/views.py
+++ institute/views.py
@@ -6,9 +6,11 @@ from institute.models import Institution
 
 def show_institutions(request):
     inst_names = Institution.objects.all()    
-    return render_to_response('institute/institutions.html', {'institutes': inst_names },
-                              context_instance=RequestContext(request))
+    return render_to_response('institute/institutions.html', 
+    	{'institutes': inst_names },
+        context_instance=RequestContext(request))
 def institution_details(request, InstId):
     institute = Institution.objects.get(id=InstId)
-    return render_to_response('institute/institutiondetails.html', {'institutes': institute },
-                              context_instance=RequestContext(request))
+    return render_to_response('institute/institutiondetails.html', 
+    	{'institutes': institute },
+        context_instance=RequestContext(request))
